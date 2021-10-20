580d5e48c3ab3d629790e3f0df6455b55382caeb
Ivy Grace M. Mapalad <73746413+IvyGraceMapalad@users.noreply.github.com>
2021-10-20 16:37:33 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,33 @@
-package package1;
+/******************************************************************************
 
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 import java.util.Scanner;
-import package2.Main_2;
-public class Main {
+public class Collatz
+{
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
 		
-		System.out.print("Enter your full name: ");
-	    String name = scan.nextLine();
-		System.out.print("Enter your age      : ");
-	    int age = scan.nextInt();
-		System.out.print("Enter your voter ID : ");
-		int id = scan.nextInt();
+		int num = 0;
+		int count = 0;
 		
-		Main_2 Obj = new Main_2(age);
-		System.out.print("\n[INFO]");
-		System.out.print("\nName: "+ name);
-		System.out.print("\nAge : "+ age);
-		System.out.print("\nID  : "+ id);
+		System.out.print("Enter a positive number: ");
+		num = scan.nextInt();
+		
+		while(num !=1){
+		    if(num % 2==0){
+		       num = num / 2; 
+		       System.out.println("Current Given value is " + num);
+		       count++;
+		    }else{
+		        num = 3 * num + 1;
+		        System.out.println("Current Given value is " + num);
+		        count++;
+		    }
+		}
 	}
 }
