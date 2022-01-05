fc1adcd45157aa0f6cbf8dd90b75af5e229063c5
Ivy Grace M. Mapalad <73746413+IvyGraceMapalad@users.noreply.github.com>
2022-01-05 16:17:00 +0800

    Create MainFinalTask_1 .java

---
+++ MainFinalTask_1 .java
@@ -0,0 +1,11 @@
+public class MainFinalTask_1 {
+	
+	public static void main(String[] args) {
+		GUI gui = new GUI();
+		gui.labels();
+		gui.textField();
+		gui.button();
+		gui.panel();
+		gui.frame();
+	}
+}
